98001bf3a5f41fc50c0d800881787ca287e8e4e7
pcr1304 <pcr130407@gmail.com>
2026-04-02 16:00:37 +0530

    add error

--- ProjX/_build/default/bin/main.ml
+++ ProjX/_build/default/bin/main.ml
@@ -3,11 +3,20 @@ open My_utils
 open Tokenizer
 open Parser
 open Pretty
+open Error
 
 let source = read_file "./input/queries.px"
 
 let () =
-  let tokens = tokenize (explode source) in
-  let ast    = parse tokens in
-  Checker.check ast;
-  print_endline (print_program ast)
+  try
+    let tokens = tokenize (explode source) in
+    let ast    = parse tokens in
+    Checker.check ast;
+    print_endline (print_program ast)
+  with
+  | ProjxError e -> 
+      print_endline (format_error e);
+      exit 1
+  | Failure msg  ->
+      print_endline msg;
+      exit 1
---
+++ ProjX/_build/default/lib/.ProjX.objs/projx__Error.impl.d
@@ -0,0 +1,1 @@
+lib/error.ml: Printf
